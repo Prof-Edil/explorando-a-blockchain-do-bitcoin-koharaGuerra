0266558f3f698d501e724c8de2c5b997830d105e
koharaGuerra <147949519+koharaGuerra@users.noreply.github.com>
2024-11-05 19:25:49 -0300

    4a

--- submission/004.sh
+++ submission/004.sh
@@ -2,4 +2,5 @@
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
 # Comando deriveaddress é usado para retornar um endereço de um descritor.
 # tr indica que o endereço a ser derivado é taproot e q4q7qurj é usado para o checksum
-bitcoin-cli deriveaddresses "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2)#q4q7qurj" [100,100]
+address=$(bitcoin-cli deriveaddresses "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2)#q4q7qurj" [100,100])
+echo $address | jq -r . '[0]' #-r serve para tirar as aspas e .'[0]' para pegar o primeiro endereço
