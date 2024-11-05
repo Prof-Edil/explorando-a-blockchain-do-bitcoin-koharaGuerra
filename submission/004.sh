a1147e65f430520c6b1d2b570abc39da44b39c88
koharaGuerra <147949519+koharaGuerra@users.noreply.github.com>
2024-11-05 20:10:24 -0300

    4A

--- submission/004.sh
+++ submission/004.sh
@@ -4,4 +4,4 @@
 # tr indica que o endereço a ser derivado é taproot e q4q7qurj é usado para o checksum
 address=$(bitcoin-cli deriveaddresses "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2/*)#q4q7qurj" [100,100])
 
-echo $address | jq -r  #-r serve para tirar as aspas e .'[0]' para pegar o primeiro endereço
+echo $address | jq -r '.[0]' #-r serve para tirar as aspas e .'[0]' para pegar o primeiro endereço
