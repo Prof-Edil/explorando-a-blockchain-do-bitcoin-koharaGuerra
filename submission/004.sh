880fb03bccb259fd95d80918c6ff542375a539be
koharaGuerra <147949519+koharaGuerra@users.noreply.github.com>
2024-11-05 19:19:38 -0300

    5

--- submission/004.sh
+++ submission/004.sh
@@ -1,2 +1,5 @@
 # Using descriptors, compute the taproot address at index 100 derived from this extended public key:
 #   `xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2`
+# Comando deriveaddress é usado para retornar um endereço de um descritor.
+# tr indica que o endereço a ser derivado é taproot e q4q7qurj é usado para o checksum
+bitcoin-cli deriveaddress "tr(xpub6Cx5tvq6nACSLJdra1A6WjqTo1SgeUZRFqsX5ysEtVBMwhCCRa4kfgFqaT2o1kwL3esB1PsYr3CUdfRZYfLHJunNWUABKftK2NjHUtzDms2)#q4q7qurj" [100,100]
