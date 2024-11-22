92621a00483ae4c38090e5b8794aef3688e4ada0
koharaGuerra <147949519+koharaGuerra@users.noreply.github.com>
2024-11-21 21:54:33 -0300

    8B

--- submission/008.sh
+++ submission/008.sh
@@ -3,8 +3,19 @@
 #!/bin/bash
 # Obtendo informações sobre o esse bloco
 raw_tx=$(bitcoin-cli getrawtransaction e5969add849689854ac7f28e45628b89f7454b83e9699e551ce14b6f90c86163 1 )
+
 script=$(echo "$raw_tx" | jq -r '.vin[0].txinwitness[2]') # Extraindo o terceiro elemento de txinwitness o qual é o redeem script
-script_decoded=$(bitcoin-cli decodescript $script) # decodifica o redeem script
-# Extrai a chave pública do scriptPubkey que assinou a primeira entrada
-public_key=$(echo $script_decoded | jq -r '.asm | split(" ") | map(select(test("^(02)[0-9a-f]{64}$")))[0]')
-echo $public_key # Imprime a chave pública
+
+verify=$(echo "$raw_tx" | jq -r '.vin[0].txinwitness[1]') # Extraindo o segundo elemento de txinwitness que indica a pubkey utilizada
+
+script_decoded=$(bitcoin-cli decodescript $script) # Decodifica o script
+# Chaves públicas indicadas no script
+public_key1=$(echo $script_decoded | jq -r '.asm | split(" ") | map(select(test("^(02|03)[0-9a-f]{64}$")))[0]')
+public_key2=$(echo $script_decoded | jq -r '.asm | split(" ") | map(select(test("^(02|03)[0-9a-f]{64}$")))[1]')
+
+if [ "$verify" == "01" ]; then #se verify for 01,
+        echo $public_key1 #A pubkey1 assinou a input 0
+
+else
+        echo $public_key2
+fi
